a7410abcf2832e343b63512a6febf8a767accdd0
Marian S <88345718+SokilMaryan@users.noreply.github.com>
2022-12-21 09:40:05 +0200

    fin1

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <el-container class="m-0 p-0 max-w[1440px] flex justify-center items-center max-h-[900px] overflow-hiden">
     <div class="grid max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-3 lg:grid-cols-12">
       <div class=" lg:col-span-7">
-        <LeftPage class="fixed" />
+        <LeftPage class="xl:fixed " />
       </div>
       <div class="mx-auto w-[680px] h-[840px] lg:mt-0 lg:col-span-5 lg:flex  rounded-[20px]" >
-        <RouterView  class="sticky-top"  />
+        <RouterView  class="xl:sticky"  />
       </div>
     </div>
   </el-container>
